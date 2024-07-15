e2284b46d8857b365df5202a987c3334eb983d3c
saibel203 <saibl.og203@gmail.com>
2024-07-15 17:21:57 +0300

    Add serilog SendGrid sink

--- src/MusShop.Api/Program.cs
+++ src/MusShop.Api/Program.cs
@@ -1,16 +1,19 @@
 using MusShop.Api;
 using MusShop.Infrastructure;
+using MusShop.Presentation.Middlewares;
+using MusShop.Presentation.Middlewares.Extensions;
 using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 ConfigurationManager configuration = builder.Configuration;
 
-builder.Services.AddInfrastructureServices(configuration);
+builder.Services.AddInfrastructureServices<ExceptionHandlerMiddleware>(configuration);
 builder.Services.AddBaseServices();
 
 WebApplication app = builder.Build();
 
 app.UseSerilogRequestLogging();
+app.UseExceptionMiddleware();
 app.UseHttpsRedirection();
 
 app.MapControllers();
--- src/MusShop.Infrastructure/InfrastructureServicesExtension.cs
+++ src/MusShop.Infrastructure/InfrastructureServicesExtension.cs
@@ -1,21 +1,27 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using System.Net;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SendGrid;
 using Serilog.Events;
 using Serilog;
+using Serilog.Filters;
+using Serilog.Sinks.Email;
 
 namespace MusShop.Infrastructure;
 
 public static class InfrastructureServicesExtension
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
-        IConfiguration configuration)
+    public static IServiceCollection AddInfrastructureServices<TExceptionMiddleware>(
+        this IServiceCollection services, IConfiguration configuration)
+        where TExceptionMiddleware : class
     {
         const string outputTemplate =
             "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
-        
+
         // Register And Setup Logger
         string logFilePath = GetLogFilePath(configuration);
-        
+        EmailConnectionInfo emailConnectionInfo = GetEmailConnectionInfoData(configuration);
+
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
@@ -35,10 +41,15 @@ public static class InfrastructureServicesExtension
                 restrictedToMinimumLevel: LogEventLevel.Information,
                 rollOnFileSizeLimit: true,
                 outputTemplate: outputTemplate)
+            .WriteTo.Logger(logger => logger
+                .Filter.ByIncludingOnly(Matching.FromSource<TExceptionMiddleware>())
+                .WriteTo.Email(
+                    connectionInfo: emailConnectionInfo,
+                    restrictedToMinimumLevel: LogEventLevel.Error))
             .CreateLogger();
 
         services.AddSerilog();
-        
+
         return services;
     }
 
@@ -59,5 +70,40 @@ public static class InfrastructureServicesExtension
             configuration[logsPath] ?? string.Empty);
 
         return logFilePath;
+    }
+
+    private static EmailConnectionInfo GetEmailConnectionInfoData(IConfiguration configuration)
+    {
+        const string fromEmailConfiguration = "SmtpErrorsConfiguration:FromEmail";
+        const string toEmailConfiguration = "SmtpErrorsConfiguration:ToEmail";
+        const string subjectEmailConfiguration = "SmtpErrorsConfiguration:ToEmail";
+        const string fromNameEmailConfiguration = "SmtpErrorsConfiguration:FromName";
+        const string portEmailConfiguration = "SmtpErrorsConfiguration:Port";
+        const string sslEmailConfiguration = "SmtpErrorsConfiguration:EnableSsl";
+        
+        const string applicationEmailPasswordConfiguration = "SmtpErrorsConfiguration:GoogleApplicationPassword";
+        const string sendGridApiKey = "SendGridOptions:ApiSecretKey";
+        
+        NetworkCredential networkCredential = new NetworkCredential
+        {
+            UserName = configuration[fromEmailConfiguration],
+            Password = configuration[applicationEmailPasswordConfiguration]
+        };
+        
+        SendGridClient sendGridClient = new SendGridClient(configuration[sendGridApiKey]);
+
+        EmailConnectionInfo emailConnectionInfo = new EmailConnectionInfo
+        {
+            EmailSubject = configuration[subjectEmailConfiguration],
+            FromEmail = configuration[fromEmailConfiguration],
+            ToEmail = configuration[toEmailConfiguration],
+            NetworkCredentials = networkCredential,
+            SendGridClient = sendGridClient,
+            FromName = configuration[fromNameEmailConfiguration],
+            Port = configuration.GetValue<int>(portEmailConfiguration),
+            EnableSsl = configuration.GetValue<bool>(sslEmailConfiguration)
+        };
+
+        return emailConnectionInfo;
     }
 }
