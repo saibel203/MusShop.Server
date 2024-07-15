8e036fa4a7e0c217476fa13b63dc966ffcffb83b
saibel203 <saibl.og203@gmail.com>
2024-07-15 17:03:12 +0300

    Add serilog file sink

--- src/MusShop.Api/Program.cs
+++ src/MusShop.Api/Program.cs
@@ -3,8 +3,9 @@ using MusShop.Infrastructure;
 using Serilog;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+ConfigurationManager configuration = builder.Configuration;
 
-builder.Services.AddInfrastructureServices();
+builder.Services.AddInfrastructureServices(configuration);
 builder.Services.AddBaseServices();
 
 WebApplication app = builder.Build();
--- src/MusShop.Infrastructure/InfrastructureServicesExtension.cs
+++ src/MusShop.Infrastructure/InfrastructureServicesExtension.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog.Events;
 using Serilog;
 
@@ -6,11 +7,15 @@ namespace MusShop.Infrastructure;
 
 public static class InfrastructureServicesExtension
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+        IConfiguration configuration)
     {
         const string outputTemplate =
             "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}";
-
+        
+        // Register And Setup Logger
+        string logFilePath = GetLogFilePath(configuration);
+        
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Debug()
             .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
@@ -24,10 +29,35 @@ public static class InfrastructureServicesExtension
             .WriteTo.Console(
                 restrictedToMinimumLevel: LogEventLevel.Debug,
                 outputTemplate: outputTemplate)
+            .WriteTo.File(
+                path: logFilePath,
+                rollingInterval: RollingInterval.Day,
+                restrictedToMinimumLevel: LogEventLevel.Information,
+                rollOnFileSizeLimit: true,
+                outputTemplate: outputTemplate)
             .CreateLogger();
 
         services.AddSerilog();
         
         return services;
+    }
+
+    private static string GetLogFilePath(IConfiguration configuration)
+    {
+        const string currentProjectName = "ApplicationNames:Infrastructure";
+        const string logsPath = "Paths:LogPath";
+
+        string workingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string? projectDirectory = Directory.GetParent(workingDirectory)?.Parent?.Parent?.Parent?.Parent?.FullName;
+
+        if (projectDirectory is null)
+        {
+            throw new DirectoryNotFoundException();
+        }
+
+        string logFilePath = Path.Combine(projectDirectory, configuration[currentProjectName] ?? string.Empty,
+            configuration[logsPath] ?? string.Empty);
+
+        return logFilePath;
     }
 }
