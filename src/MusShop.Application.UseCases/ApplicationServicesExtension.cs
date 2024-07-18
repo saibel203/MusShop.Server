49354562762f9e93f00b3461ecae89d86dde3a49
saibel203 <saibl.og203@gmail.com>
2024-07-18 17:43:11 +0300

    Add CQRS pattern and logging pipeline

---
+++ src/MusShop.Application.UseCases/ApplicationServicesExtension.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using MusShop.Application.UseCases.Commons.Behaviors;
+
+namespace MusShop.Application.UseCases;
+
+public static class ApplicationServicesExtension
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        // Add Mediator pattern
+        services.AddMediatR(config =>
+            config.RegisterServicesFromAssemblies(AppDomain.CurrentDomain.GetAssemblies()));
+
+        // Add Mediator behaviors
+        services.AddScoped(typeof(IPipelineBehavior<,>), typeof(LoggingPipelineBehavior<,>));
+
+        return services;
+    }
+}
---
+++ src/MusShop.Application.UseCases/Commons/Behaviors/LoggingPipelineBehavior.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Serilog;
+
+namespace MusShop.Application.UseCases.Commons.Behaviors;
+
+public class LoggingPipelineBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly ILogger _logger = Log.ForContext<LoggingPipelineBehavior<TRequest, TResponse>>();
+
+    public async Task<TResponse> Handle(
+        TRequest request,
+        RequestHandlerDelegate<TResponse> next,
+        CancellationToken cancellationToken)
+    {
+        string requestName = typeof(TRequest).Name;
+        
+        _logger.Information(
+            "Processing request {RequestName}", requestName);
+
+        TResponse result = await next();
+        
+        return result;
+    }
+}
